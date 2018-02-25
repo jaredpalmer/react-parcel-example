987d0d14f71de401443cbf298eddf801731858dc
SawWu <2804265880@qq.com>
2018-02-25 13:53:17 +0800

    增加处理样式

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
-import './index.css';
+import './index.scss';
 
 const App = () => (
   <div className="App">
