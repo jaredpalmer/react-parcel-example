f1f88682539bbd7ce9ecd9e18adeedef2463b964
SawWu <2804265880@qq.com>
2018-02-25 21:45:57 +0800

    更新組件

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+const App = () => (
+    <div className="App">
+        <img className="App-Logo" src={logo} alt="React Logo" />
+        <h1 className="App-Title">Hello Parcel x React</h1>
+    </div>
+);
+
+export default App;
